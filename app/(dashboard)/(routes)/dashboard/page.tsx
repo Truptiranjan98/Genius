f7aba1d3e59dee81e1b1280b1e2de1407f1a0c5d
Truptiranjan Biswal <truptiranjanbiswal123@gmail.com>
2023-12-17 08:12:29 +0530

    first commit

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+
+const DashboardPage = () => {
+  return (
+    <div>
+        <p>
+      Dashboard Page (protected) 
+      {/* only authorized person can see this page */}
+      </p>
+     
+    </div>
+  )
+}
+
+export default DashboardPage
+
+
